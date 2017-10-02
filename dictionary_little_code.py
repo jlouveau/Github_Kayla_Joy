66448bb93ae454e1718588ede67b6cade5049a15
jlouveau <jlouveau@dhcp-18-189-115-31.dyn.MIT.EDU>
2017-10-02 17:29:23 -0400

    Simulation notebook to generate figures
    Mixture_project notebook after we converged

--- dictionary_little_code.py
+++ dictionary_little_code.py
@@ -3,47 +3,57 @@ import csv
 
 ####### parameters #######
 time_Steps = [2, 61, 121, 181, 241]
-singleAntigenTest = 1
+singleAntigenTest = 0
+
 cocktailFirst = 1
-l = 12 #number of residues, 3 conserved, 9 variable
+l = 30 #number of residues, 3 conserved, 9 variable
+
+conccock=1.05
+concseq=1.15
 
 ####### antigens creation ####### 
-#initializer
-Ag0 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+#initializer2
+Ag0 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
  
-# closely related antigens d = 1
-Ag1 = [-1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-Ag2 = [1, 1, 1, 1, -1, 1, 1, 1, 1, 1, 1, 1]
-Ag3 = [1, 1, 1, 1, 1, 1, 1, 1, -1, 1, 1, 1]
+# closely related antigens d = 5
+Ag1 = [-1, -1, -1, -1, -1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1]
+Ag2 = [ 1,  1,  1,  1,  1, -1, -1, -1, -1, -1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1]
+Ag3 = [ 1,  1,  1,  1,  1,  1,  1,  1,  1,  1, -1, -1, -1, -1, -1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1]
 
-# distant antigens for sequential D = 3
-Ag4 = [-1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-Ag5 = [1, 1, 1, -1, -1, -1, 1, 1, 1, 1, 1, 1]
-Ag6 = [1, 1, 1, 1, 1, 1, -1, -1, -1, 1, 1, 1]
+# distant antigens for sequential D = 15
+Ag4 = [ 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
+Ag5 = [ 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, -1, -1, -1, -1, -1, -1, -1,  1,  1,  1,  1]
+Ag6 = [ 1,  1,  1, -1,  1,  1,  1, -1,  1,  1,  1, -1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, -1, -1, -1, -1]
+
 
 if singleAntigenTest ==1:
-    dicAgs = {c:Ag0 for c in list(range(time_Steps[0], time_Steps[3]))}
-    with open('output_singleAntigenDict.csv', 'w') as output:
-        w = csv.writer(output)
-        w.writerows(dicAgs.items())
+        dicAgs = {c:Ag0 for c in list(range(time_Steps[0], time_Steps[3]))}
+        dicconc= {c:conccock for c in list(range(time_Steps[0], time_Steps[3]))}
+        with open('output_singleAntigenDict.csv', 'w') as output:
+            w = csv.writer(output)
+            w.writerows(dicAgs.items())
 else:        
-    if cocktailFirst==1: 
-        dicCocktail = {c: [Ag0, Ag1, Ag2] for c in list(range(time_Steps[0],time_Steps[1]))}
-        dicAgs = dicCocktail.copy()
-        dicAgs.update({c:Ag4 for c in list(range(time_Steps[1],time_Steps[2]))})
-        dicAgs.update({c:Ag5 for c in list(range(time_Steps[2],time_Steps[3]))})
-        dicAgs.update({c:Ag6 for c in list(range(time_Steps[3],time_Steps[4]))})
-        with open('output_cocktailDict.csv', 'w') as output:
-            w = csv.writer(output)
-            w.writerows(dicAgs.items())
+        if cocktailFirst==1: 
+            dicCocktail = {c: [Ag0, Ag1, Ag2, Ag3] for c in list(range(time_Steps[0],time_Steps[3]))}
+            dicAgs = dicCocktail.copy()
+   #         dicAgs.update({c:Ag4 for c in list(range(time_Steps[1],time_Steps[2]))})
+   #        dicAgs.update({c:Ag5 for c in list(range(time_Steps[2],time_Steps[3]))})
+   #         dicAgs.update({c:Ag6 for c in list(range(time_Steps[3],time_Steps[4]))})
+            dicconc= {c:conccock for c in list(range(time_Steps[0], time_Steps[3]))}
+   #        dicconc.update({c:concseq for c in list(range(time_Steps[1],time_Steps[2]))})
+   #         dicconc.update({c:concseq for c in list(range(time_Steps[2],time_Steps[3]))})
+   #         dicconc.update({c:concseq for c in list(range(time_Steps[3],time_Steps[4]))})
+            with open('output_cocktailDict.csv', 'w') as output:
+                w = csv.writer(output)
+                w.writerows(dicAgs.items())
     
-    else:      
-        dicAgs = {c:Ag0 for c in list(range(time_Steps[0],time_Steps[1]))}
-        dicAgs.update({c:Ag4 for c in list(range(time_Steps[1],time_Steps[2]))})
-        dicAgs.update({c:Ag5 for c in list(range(time_Steps[2],time_Steps[3]))})
-        dicCocktail = {c: [Ag1, Ag2, Ag3] for c in list(range(time_Steps[3],time_Steps[4]))}
-        dicAgs.update(dicCocktail)    
-        with open('output_sequenceDict.csv', 'w') as output:
-            w = csv.writer(output)
-            w.writerows(dicAgs.items())
-    
+        else:      
+            dicAgs = {c:Ag0 for c in list(range(time_Steps[0],time_Steps[1]))}
+            dicAgs.update({c:Ag4 for c in list(range(time_Steps[1],time_Steps[2]))})
+            dicAgs.update({c:Ag5 for c in list(range(time_Steps[2],time_Steps[3]))})
+            dicCocktail = {c: [Ag1, Ag2, Ag3] for c in list(range(time_Steps[3],time_Steps[4]))}
+            dicAgs.update(dicCocktail)    
+            with open('output_sequenceDict.csv', 'w') as output:
+                w = csv.writer(output)
+                w.writerows(dicAgs.items())
+
